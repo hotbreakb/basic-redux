8887d80b010a704a12430256ec1f3273fbe0f84c
hotbreakb <areyhappy12@gmail.com>
2022-04-22 21:50:17 +0900

    add todo reducer

--- src/modules/rootReducer.ts
+++ src/modules/rootReducer.ts
@@ -1,8 +1,10 @@
 import { combineReducers } from "@reduxjs/toolkit";
 import counter from "./counter/counter";
+import todos from "./todos/todos";
 
 const rootReducer = combineReducers({
     counter,
+    todos,
 });
 
 export default rootReducer;
