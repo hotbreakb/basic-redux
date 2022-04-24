903c1f149f18adbc601e90f6d3ab20cd8551f658
hotbreakb <areyhappy12@gmail.com>
2022-04-24 16:53:41 +0900

    add posts for redux-thunk

---
+++ src/modules/posts/posts.ts
@@ -0,0 +1,37 @@
+import * as postsAPI from '../../api/posts';
+import { createPromiseThunk, handleAsyncActions, reducerUtils } from '../../lib/asyncUtils';
+
+/* 액션 타입 */
+
+// 포스트 여러개 조회하기
+const GET_POSTS = 'GET_POSTS'; // 요청 시작
+const GET_POSTS_SUCCESS = 'GET_POSTS_SUCCESS'; // 요청 성공
+const GET_POSTS_ERROR = 'GET_POSTS_ERROR'; // 요청 실패
+
+// 포스트 하나 조회하기
+const GET_POST = 'GET_POST';
+const GET_POST_SUCCESS = 'GET_POST_SUCCESS';
+const GET_POST_ERROR = 'GET_POST_ERROR';
+
+export const getPosts = createPromiseThunk(GET_POSTS, postsAPI.getPosts);
+export const getPost = createPromiseThunk(GET_POSTS, postsAPI.getPostById);
+
+const initialState = {
+    posts: reducerUtils.initial(),
+    post: reducerUtils.initial(),
+};
+
+export default function posts(state = initialState, action: any) {
+    switch (action.type) {
+        case GET_POSTS:
+        case GET_POSTS_SUCCESS:
+        case GET_POSTS_ERROR:
+            return handleAsyncActions(GET_POSTS, 'posts')(state, action);
+        case GET_POST:
+        case GET_POST_SUCCESS:
+        case GET_POST_ERROR:
+            return handleAsyncActions(GET_POST, 'post')(state, action);
+        default:
+            return state;
+    }
+}
--- src/modules/store.ts
+++ src/modules/store.ts
@@ -1,13 +1,16 @@
 import { configureStore } from '@reduxjs/toolkit';
-import myLogger from '../middlewares/myLogger';
 import counter from './counter/counter';
 import { todosSlice } from './todos/todos';
 import { applyMiddleware, combineReducers, createStore } from "redux";
 import { logger } from "redux-logger";
+import { composeWithDevTools } from 'redux-devtools-extension';
+import ReduxThunk from "redux-thunk";
+import posts from './posts/posts';
 
 const reducers = {
     counter,
-    todos: todosSlice.reducer
+    todos: todosSlice.reducer,
+    posts,
 }
 
 const rootReducer = combineReducers(reducers);
@@ -16,7 +19,10 @@ const rootReducer = combineReducers(reducers);
 //     reducer: reducers,
 // });
 
-export const store = createStore(rootReducer, applyMiddleware(myLogger, logger));
+export const store = createStore(
+    rootReducer,
+    composeWithDevTools(applyMiddleware(ReduxThunk, logger))
+);
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
